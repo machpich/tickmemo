51e3d71be6d37669160ac8b2be08dd75748c26d8
shkobm <scrt.8shbabe@gmail.com>
2018-03-17 14:58:23 +0900

    [fix]kaminari

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,44 +2,13 @@ class EventsController < ApplicationController
   before_action :authenticate_user!
   after_action :clean_memo, only:[:update]
 
-  # def create
-  #   # イベントと場所の登録
-  #   @event = Event.new(params_event)
-  #   @event.user = current_user
-  #   if Event.find(params[:event_name]).empty?
-  #     @event.save
-  #   else
-  #     event_not_saved = true
-  #   end
-
-  #   #相手先情報の登録
-  #   @otherside = Otherside.new(params_otherside)
-  #   @otherside.user = current_user
-  #   @otherside.save
-
-  #   # 場所の登録
-  #   @location = Location.new(params_location)
-  #   @location.event = @event
-  #   @location.save
-
-  #   #スケジュールの登録
-  #   @schedule = @event.schedules.build(params_schedule)
-  #     @schedule.location = @location
-  #     @schedule.otherside = @otherside
-  #     @schedule.user = current_user
-  #     @schedule.save
-
-  #   redirect_to index_path
-  # end
-
   def index
-    # @schedules = Schedule.where(user_id:current_user.id,event_id:params[:id]).order(start_datetime: :desc)
     @events = Event.where(user_id: current_user.id).order(created_at: :desc)
   end
 
   def show
     @event = Event.find(params[:id])
-    @schedules = Schedule.where(user_id:current_user.id,event_id:params[:id]).order(:start_datetime)
+    @schedules = Schedule.where(user_id:current_user.id,event_id:params[:id]).order(:start_datetime).page(params[:page])
   end
 
   def edit
